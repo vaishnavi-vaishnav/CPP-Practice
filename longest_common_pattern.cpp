e8f0501d5a1cb7af3cfe644fcbf7f5e3384a70ac
vaishnavi-vaishnav <vaishnavivaishnav0000@gmail.com>
2023-04-04 15:16:38 +0530

    commit

---
+++ longest_common_pattern.cpp
@@ -0,0 +1,25 @@
+// As we all know, Chef is cooking string for long days, his new discovery 
+// on string is the longest common pattern length. The longest common pattern 
+// length between two strings is the maximum number of characters that both 
+// strings have in common. Characters are case sensitive, that is, lower case 
+// and upper case characters are considered as different. Note that characters 
+// can repeat in a string and a character might have one or more occurrence in 
+// common between two strings. For example, if Chef has two strings 
+// A = "Codechef" and B = "elfedcc", then the longest common pattern 
+// length of A and B is 5 (common characters are c, d, e, e, f).
+
+// Chef wants to test you with the problem described above. He will give 
+// you two strings of Latin alphabets and digits, return him the longest 
+// common pattern length.
+
+// Input:
+// The first line of the input contains an integer T, denoting the number 
+// of test cases. Then the description of T test cases follows.
+// The first line of each test case contains a string A. The next line 
+// contains another character string B.
+
+// Output:
+// For each test case, output a single line containing a single 
+// integer, the longest common pattern length between A and B.
+
+
---
+++ strong_language.cpp
@@ -0,0 +1,22 @@
+// A string is said to be using strong language if it contains at least 
+// K consecutive characters '*'.
+// You are given a string S with length N. Determine whether it uses 
+// strong language or not.
+
+// Input
+// The first line of the input contains a single integer T denoting the 
+// number of test cases. The description of T test cases follows.
+// The first line of each test case contains two space-separated integers 
+// N and K.
+// The second line contains a single string S with length N.
+
+// Output
+// Print a single line containing the string "YES" if the string contains 
+// strong language or "NO" if it does not (without quotes).
+
+// You may print each character of each string in uppercase or lowercase 
+// (for example, the strings "yEs", "yes", "Yes" and "YES" will all be treated as identical).
+
+
+
+
