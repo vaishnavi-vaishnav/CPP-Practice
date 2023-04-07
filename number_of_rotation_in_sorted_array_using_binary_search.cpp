a0491e90b245bf720a1355c2f9ba6930a22a807e
vaishnavi-vaishnav <vaishnavivaishnav0000@gmail.com>
2023-04-07 15:15:56 +0530

    updating K rotation file

--- number_of_rotation_in_sorted_array_using_binary_search.cpp
+++ number_of_rotation_in_sorted_array_using_binary_search.cpp
@@ -4,16 +4,16 @@
  class solution{
 
  public:
-     int findkrotation(int arr[], int n){
+     int rotation(int arr[], int n){
 
-  int low = 0, high = n - 1;
-    while (low <= high) {
-        if (arr[low] <= arr[high]) {
+  int left = 0, right = n - 1;
+    while (left <= right) {
+        if (arr[left] <= arr[right]) {
             // Case 1: Array is already sorted
-            return low;
+            return left;
         }
         
-        int mid = ( low + high ) / 2;
+        int mid = ( left + right ) / 2;
         int next = mid + 1;
         int prev = mid - 1;
         
@@ -21,13 +21,13 @@
             // Case 2: Mid element is the minimum element
             return mid;
         }
-        else if (arr[mid] <= arr[high]) {
+        else if (arr[mid] <= arr[right]) {
             // Case 3: Right half is sorted, search left half
-            high = mid - 1;
+            right = mid - 1;
         }
-        else if (arr[mid] >= arr[low]) {
+        else if (arr[mid] >= arr[left]) {
             // Case 4: Left half is sorted, search right half
-            low = mid + 1;
+            left = mid + 1;
         }
     }
     return -1; // Not found
@@ -39,15 +39,17 @@ int main(){
     // cin >> t;
     // while(t--){
         int n;
+        cout << "Enter the size of the array: ";
         cin >> n;
         int a[n];
 
         for(int i = 0; i < n; i++){
+            cout <<"Enter " << i << " element: ";
             cin >> a[i];
         }
 
         solution ob;
-        auto ans = ob.findkrotation(a,n);
+        auto ans = ob.rotation(a,n);
         cout << ans << "\n";
 
 
