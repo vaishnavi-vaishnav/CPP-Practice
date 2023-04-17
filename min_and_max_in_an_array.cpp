f73028ff3cc78c712c1e6ff4aebc9644863c70b0
vaishnavi-vaishnav <vaishnavivaishnav0000@gmail.com>
2023-04-17 14:04:09 +0530

    Reverse an array using different methods

--- min_and_max_in_an_array.cpp
+++ min_and_max_in_an_array.cpp
@@ -1,31 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Method - 1 (Using Sorting)
-
-// int main(){
-
-//     int n;
-//     cout << "Enter the size of the array: ";
-//     cin >> n;
-//     int arr[n];
-    
-//     for(int i = 0; i < n; i++){
-//        cin >> arr[i];
-//     }
-
-//     sort(arr, arr + n);
-
-//     cout << "Minimum element: " << arr[0] << endl;
-//     cout << "Maximum element: " << arr[n - 1] << endl;
-
-//     return 0;
-
-// }
-
-// Method - 1 (Using linear search)
-
-
+//Method - 1
 int UsingSorting(int arr[], int n){
 
     sort(arr, arr + n);
@@ -34,7 +10,12 @@ int UsingSorting(int arr[], int n){
     cout << "Maximum element: " << arr[n - 1] << endl;
 
 }
+// The time complexity of this approach is O(n log n), where n is the number of elements in the array, 
+// as we are using a sorting algorithm. The space complexity is O(1), as we are not using any extra space.
 
+
+
+//Method - 2
 int UsingLinearSearch(int arr[], int n){
 
     int min = arr[0];
@@ -53,6 +34,9 @@ int UsingLinearSearch(int arr[], int n){
     cout << "Maximum Element: " << max << endl;
 
 }
+
+// Time Complexity: O(n)
+// Auxiliary Space: O(1) as no extra space was needed.
 
 
 int main(){
---
+++ reverse_an_array.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void IterativeMethod(int arr[], int start, int end){
+
+    while(start < end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+}
+
+void RecursiveMethod(int arr[], int start, int end){
+    if(start >= end)
+        return;
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    RecursiveMethod(arr, start+1, end-1);
+}
+
+void PrintArray(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    cout << "Iterative method to reverse an array: " << endl; 
+    IterativeMethod(arr, 0, n-1);
+    PrintArray(arr, n);
+
+    cout << "Recursive method to reverse an array: " << endl; 
+    RecursiveMethod(arr, 0, n-1);
+    PrintArray(arr, n);
+
+    return 0;
+
+}
