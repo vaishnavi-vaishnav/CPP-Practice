185fe98e940f323cec452dfc215b70b7fffba4bf
vaishnavi-vaishnav <vaishnavivaishnav0000@gmail.com>
2023-04-24 15:55:19 +0530

    left and right shift questions

---
+++ left_and_right_shift.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int a = 10, b = 1;
+    // if(++a){
+    //     cout << b;
+    // }
+    // else{
+    //     cout << ++b;
+    // }
+
+    // int a = 1;
+    // int b = 2;
+    // if(a-- > 0 && ++b > 2){
+    //     cout << "stage-1 inside if" <<  endl;
+    // }
+    // else{
+    //     cout << "stage-2 inside else" << endl;
+    // }
+    // cout << a << " " << b << endl;
+
+
+    // int number = 3;
+    // cout << (25*(++number));
+
+
+    
+
+    return 0 ;
+}
